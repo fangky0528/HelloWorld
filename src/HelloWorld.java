8dabd9d118b215ba2ee3240b0d3e01508652ae20
fangky0528 <manchester0710@sina.com>
2018-05-30 10:28:57 +0800

    test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         String str = "123";
+        String str1 = "123444445555";
     }
 }
