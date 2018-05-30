6965a5bf8359fa3bd7979e58f4b571352bda3681
fangky0528 <manchester0710@sina.com>
2018-05-30 12:42:13 +0800

    sd

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
     public int s1;
 
     public static void main(String[] args) {
-        String str2 = "123111";
+        String str3 = "123111";
         Integer i = 12;
         i = i / 6;
         System.out.println("i = " + i);
