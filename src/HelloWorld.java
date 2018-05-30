8a5268da89c6ec38da0cf90b1c8c74dfb5ba98bd
fangky0528 <manchester0710@sina.com>
2018-05-30 12:54:52 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,11 @@
 public class HelloWorld {
 
-    String str5 = "12222243111";
+    String str1 = "12222243111";
+    String str2 = "11";
     public int s1;
 
     public static void main(String[] args) {
-        String str3 = "123111";
+        String str2 = "123111";
         Integer i = 12;
         i = i / 6;
         System.out.println("i = " + i);
