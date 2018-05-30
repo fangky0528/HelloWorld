d1fb9d838d4419572eca99927858f857dbf9cdec
fangky0528 <manchester0710@sina.com>
2018-05-30 11:34:23 +0800

    sd

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,17 @@
 public class HelloWorld {
 
-    public String s1;
-
+    public int s1;
 
     public static void main(String[] args) {
         String str = "123";
+        Integer i = 12;
+        i = i / 6;
+        System.out.println("i = " + i);
         System.out.println("Hello World");
+
+    }
+
+    public String getMsg() {
+        return "testAA";
     }
 }
